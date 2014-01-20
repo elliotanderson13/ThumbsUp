77641028fc184052c5575fdf7e057a76ad0529a8
elliotanderson13 <pennyanderson@Elliots-MacBook-Pro.local>
2014-01-20 18:04:48 -0500

    Fixed email template link

--- app/views/email.blade.php
+++ app/views/email.blade.php
@@ -2,7 +2,7 @@ Hey {{ $name }},
 
 <br><br>
 
-{{ $poster }} has just thanked you! Go to http://108.16.18.76/ThumbsUp/public/wall to view your thanks.
+{{ $poster }} has just thanked you! Go to {{url('wall')}} to view your thanks.
 
 <br><br>
 
